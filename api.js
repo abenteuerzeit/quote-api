a2cd98850d8a93dfb19dcc545b9663a0029d9261
abenteuerzeit <amroz.muzyka@gmail.com>
2023-01-25 21:24:29 +0100

    create get quotes route

--- api.js
+++ api.js
@@ -4,4 +4,10 @@ const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
 
+apiRouter.get('/quotes', (req, res, next) => {
+    res.status(200).json({ quotes: quotes });
+});
+
+
+
 module.exports = apiRouter;
